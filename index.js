f75f937da7d8ff3016d337027710611039e4b373
Shelex <ovr.shevtsov@gmail.com>
2022-03-20 23:05:30 +0200

    fix: import path for cypress run

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const SpecSplitClient = require('./splitter');
 const filesToSpecInput = require('./filereader');
-const runCypress = require('./cy-run');
+const runCypress = require('./cy-run/runner');
 
 module.exports = {
     SpecSplitClient,
